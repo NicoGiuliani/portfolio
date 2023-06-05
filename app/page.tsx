3948eaf4566c0f17dcb9231ed43278f070bb7b2c
Nico Julian <NicoGiuliani@users.noreply.github.com>
2023-06-05 10:05:05 -0700

    More layout changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
+import { motion } from "framer-motion";
 import { Suspense } from "react";
 import { GiRattlesnake } from "react-icons/gi";
 
@@ -42,20 +43,22 @@ export default function Home() {
         <Contact />
       </section>
 
-      {/* <footer className="overflow-x-hidden footer-element">
-        <div className="flex">
-          <a href="#top">
-            <GiRattlesnake className="h-12 w-12 text-gray-400 hover:text-[#68f0be]" />
-          </a>
-        </div>
-      </footer> */}
-
       <footer  className="sticky bottom-10 xl:bottom-0 flex flex-start justify-between items-center max-w-7xl mx-auto p-5 z-20">
-        <div className="ml-auto">
+        <motion.div 
+          initial={{
+            opacity: 0
+          }}
+          whileInView={{
+            opacity: 1
+          }}
+          transition={{
+            duration: 1.5
+          }}
+          className="ml-auto">
           <a href="#top">
             <GiRattlesnake className="h-12 w-12 text-gray-400 hover:text-[#68f0be]" />
           </a>
-        </div>
+        </motion.div>
       </footer>
 
     </main>
--- components/Header.tsx
+++ components/Header.tsx
@@ -10,17 +10,15 @@ export default function Header({ }: Props) {
     <header className="sticky top-0 flex flex-start justify-between items-center max-w-7xl mx-auto p-5 z-20">
       <motion.div
         initial={{
-          x: -120,
           opacity: 0
         }}
         whileInView={{
-          x: 0,
           opacity: 1
         }}
         transition={{
           duration: 1.5
         }}
-        className='flex flex-row gap-2 items-center w-[120px]'
+        className='flex flex-row gap-2 items-center'
       >
         <SocialIcon url="https://www.linkedin.com/in/nico-julian/" fgColor="gray" bgColor="transparent" />
         <SocialIcon url="https://github.com/NicoGiuliani" fgColor="gray" bgColor="transparent" />
@@ -30,17 +28,15 @@ export default function Header({ }: Props) {
       <a href="#contact">
         <motion.div
           initial={{
-            x: 160,
             opacity: 0
           }}
           whileInView={{
-            x: 0,
             opacity: 1
           }}
           transition={{
             duration: 1.5
           }}
-          className='flex flex-row space-x-2 items-center cursor-pointer w-[25px] sm:w-[152px] 2xl:w-[159px]'
+          className='flex flex-row space-x-2 items-center cursor-pointer'
         >
           <MdOutlineEmail className='w-[25px] h-[25px] text-gray-400'/>
           <p className='uppercase text-sm 2xl:text-base text-gray-400 hidden sm:inline-flex tracking-[10px] ml-[15px]'>Contact</p>
